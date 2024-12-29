5c5418d0d6e1e4aa1b5adb5c681da824ae415dbb
ggomezl01 <123774050+ggomezl01@users.noreply.github.com>
2024-12-29 20:03:19 +0100

    Mejora pokémon

--- pokemon/script.js
+++ pokemon/script.js
@@ -1,32 +1,51 @@
 var pulsaciones = 0;
 var carta1 = '';
 var carta2 = '';
+var bloqueado = false;
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll(".carta").forEach(element => {
-        element.addEventListener('click', function(){
-            if(!element.classList.contains('seleccionada')){
-                element.classList.toggle('seleccionada');
-                pulsaciones++;
-                if(pulsaciones==1){
-                    carta1 = element.classList[1];
-                }else if(pulsaciones==2){
-                    carta2 = element.classList[1];
-                    pulsaciones=0;
-                    if(carta1 != carta2){
-                        setTimeout(quitarClaseSeleccionada, 700);
-                    };
-                };
-            };
+        element.addEventListener('click', function () {
+            if (bloqueado || element.classList.contains('seleccionada')) {
+                return;
+            }
+
+            element.classList.toggle('seleccionada');
+            pulsaciones++;
+
+            if (pulsaciones === 1) {
+                carta1 = element;
+            } else if (pulsaciones === 2) {
+                carta2 = element;
+                bloqueado = true;
+
+                if (carta1.classList[1] !== carta2.classList[1]) {
+                    setTimeout(quitarClaseSeleccionada, 700);
+                } else {
+                    carta1.classList.toggle('correcta');
+                    carta2.classList.toggle('correcta');
+                    bloqueado = false;
+
+                    if(comprobarFinal()){
+                        const confetti = document.getElementById("confetti");
+                        confetti.removeAttribute("hidden");
+                    }
+                }
+                pulsaciones = 0;
+            }
         });
     });
 });
 
-function quitarClaseSeleccionada(){
-    document.querySelectorAll("." + carta1).forEach(element => {
-        element.classList.remove('seleccionada');
+function quitarClaseSeleccionada() {
+    document.querySelectorAll(".seleccionada").forEach(element => {
+        if(!element.classList.contains('correcta')){
+            element.classList.remove('seleccionada');
+        }
     });
-    document.querySelectorAll("." + carta2).forEach(element => {
-        element.classList.remove('seleccionada');
-    });
+    bloqueado = false;
+}
+
+function comprobarFinal() {
+    return document.querySelectorAll(".carta.correcta").length === document.querySelectorAll(".carta").length;
 }
